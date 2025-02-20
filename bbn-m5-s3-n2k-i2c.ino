3e31d86294d5e78581d2cf143012016161005f5a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-20 07:45:20 -0500

    Update bbn-m5-s3-n2k-i2c.ino

--- bbn-m5-s3-n2k-i2c.ino
+++ bbn-m5-s3-n2k-i2c.ino
@@ -65,9 +65,9 @@ static bool led_state = false;
 
 void ToggleLed() {
   if (led_state) {
-    M5.dis.drawpix(0x00ff00);
+    M5.dis.drawpix(0, 0x00ff00);
   } else {
-    M5.dis.drawpix(0x000000);
+    M5.dis.drawpix(0, 0x000000);
   }
   M5.update();
   led_state = !led_state;
