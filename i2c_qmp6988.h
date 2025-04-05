8de5aaed8233c7dd8f40891f7efe360fb783fcd9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-05 17:18:49 -0400

    Update i2c_qmp6988.h

--- i2c_qmp6988.h
+++ i2c_qmp6988.h
@@ -53,7 +53,7 @@ bool i2c_qmp6988_try_init() {
   }
   if (i2c_qmp6988_found) {
     gen_nmea0183_msg("$BBTXT,01,01,01,ENVIRONMENT found qmp6988 sensor at address=0x%s", String(QMP6988_SLAVE_ADDRESS_L, HEX).c_str());
-    app.onRepeat(5000, []() {
+    app.onRepeat(250, []() {
       i2c_qmp6988_report();
     });
   }
