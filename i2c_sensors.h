8fb7698686dbf69cae4e60b61848b2100c0fe0b2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-04-05 17:47:06 -0400

    Update i2c_sensors.h

--- i2c_sensors.h
+++ i2c_sensors.h
@@ -4,7 +4,7 @@
 #include "i2c_sht30.h"
 #include "i2c_qmp6988.h"
 
-void i2c_sensors_scan(bool i2c_alt_enable_scan) {
+void i2c_sensors_scan() {
   i2c_sht30_try_init();
   i2c_qmp6988_try_init();
 }
